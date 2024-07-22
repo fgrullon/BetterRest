931e5a7e9a854abfe81e584f3e600c6118dd0f9b
fgrullon <franfelix@gmail.com>
2024-07-22 18:18:15 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Frank Grullon on 22/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
