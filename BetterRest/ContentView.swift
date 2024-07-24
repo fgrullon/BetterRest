6ee9477a2d746f71b347540ceddeac8dbfadd86b
fgrullon <franfelix@gmail.com>
2024-07-24 18:45:39 -0400

    changes in style, vstack to sections

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -11,31 +11,40 @@ import SwiftUI
 struct ContentView: View {
     @State private var coffeAmount = 1
     @State private var SleepAmount = 8.0
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeUpTime
     
     @State private var showAlert = false
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     
+    static var defaultWakeUpTime : Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        
+        return Calendar.current.date(from: components) ?? .now
+    }
+        
     var body: some View {
         NavigationStack{
-            VStack{
+            Form{
                 
-            Text("What time do yo want to wake up")
-                .font(.headline)
-            
-            DatePicker("Time to wake up", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                .labelsHidden()
-            
-            Text("Desired amount of sleep")
-                .font(.headline)
-            
-            Stepper("\(SleepAmount.formatted()) hours", value: $SleepAmount, in: 4...12, step: 0.25)
-            
-            Text("Daily coffe intake")
-                .font(.headline)
-            
-            Stepper("\(coffeAmount)", value: $coffeAmount, in: 1...20)
+                Section("What time do yo want to wake up"){
+                
+                    
+                    DatePicker("Time to wake up", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                .bold()
+
+                Section("Desired amount of sleep"){
+                    Stepper("\(SleepAmount.formatted()) hours", value: $SleepAmount, in: 4...12, step: 0.25)
+                }
+                .bold()
+                Section("Daily coffe intake") {
+                    Stepper(coffeAmount == 1 ? "1 cup" : "\(coffeAmount) cups", value: $coffeAmount, in: 1...20)
+                }
+                .bold()
         }
             .navigationTitle("BetterRest")
             .toolbar{
