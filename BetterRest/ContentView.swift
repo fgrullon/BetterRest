ae21864451379f9f5c53147f15c5864fae16d5fa
fgrullon <franfelix@gmail.com>
2024-07-23 20:34:29 -0400

    Calculate sleep time using trained model and display on ui

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,12 +5,18 @@
 //  Created by Frank Grullon on 22/7/24.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var coffeAmount = 1
     @State private var SleepAmount = 8.0
     @State private var wakeUp = Date.now
+    
+    @State private var showAlert = false
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    
     var body: some View {
         NavigationStack{
             VStack{
@@ -35,12 +41,38 @@ struct ContentView: View {
             .toolbar{
                 Button("Calculate", action: calculateBedTime)
             }
+            .alert(alertTitle, isPresented: $showAlert){
+                Button("Ok") {}
+            } message: {
+                Text(alertMessage)
+            }
         }
         .padding()
     }
     
     func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: SleepAmount, coffee: Double(coffeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            alertTitle = "Error!"
+            alertMessage = "Sorry, there was a problem calculating your bedtime"
+        }
         
+        showAlert = true
     }
 }
 
